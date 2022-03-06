b57bf03a32c7614c8bfb4708459510efd84b36e5
Misho <nnamper@gmail.com>
2022-03-06 13:11:15 +0400

    Inital setup for serializiation

--- avoid-irs-back/src/main.rs
+++ avoid-irs-back/src/main.rs
@@ -1,13 +1,36 @@
-#[macro_use] extern crate rocket;
+use rocket::{
+    self,
+    serde::{json::Json, Deserialize, Serialize},
+};
 
-#[get("/")]
-fn index() -> &'static str{
-    "Hello, world!"
+
+#[derive(Deserialize)]
+struct IncomeIn{
+    currency: String,
+    amount: f64
+}
+
+#[derive(Serialize)]
+struct IncomeOut{
+    value: f64
+}
+
+#[rocket::post("/api/monthly_tax", format = "json",  data="<income>")]
+fn monthly_tax(income: Json<IncomeIn>) -> Json<IncomeOut>{
+    // montlhy tax computation
+    return Json(IncomeOut { value : income.amount });
 }
 
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+#[rocket::main]
+async fn main(){
+    if let Err(err) = rocket::build()
+        .mount("/", rocket::routes![monthly_tax])
+        .launch()
+        .await
+    {
+       println!("Rocket Rust couldn't take off successfully!");
+       drop(err); // Drop initiates Rocket-formatted panic
+    }
 }
 
