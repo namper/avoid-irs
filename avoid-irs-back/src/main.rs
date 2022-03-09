ea2b5f0d3194017bcefeb9c556463565cd44cc5e
Misho <nnamper@gmail.com>
2022-03-10 00:30:23 +0400

    GE Rs Reqwuest Handling

--- avoid-irs-back/src/main.rs
+++ avoid-irs-back/src/main.rs
@@ -3,17 +3,53 @@ use rocket::{
     serde::{json::Json, Deserialize, Serialize},
 };
 
+use reqwest::{
+    self,
+}
+
+const URL: string = "https://nbg.gov.ge/gw/api/ct/monetarypolicy/currencies/";
 
 #[derive(Deserialize)]
 struct IncomeIn{
-    currency: String,
-    amount: f64
+    currency: std::Option<String>,
+    amount: f64,
 }
 
 #[derive(Serialize)]
 struct IncomeOut{
     value: f64
 }
+
+#[derive(Deserialize)]         
+struct RSCurrencyRateResponse{
+    currencies: str:Vect<{validFromDate: String, code: String}
+}
+
+fn get_currency_rate(date: str) -> f64:
+    // get official curreny on given date
+    // @TODO: Convert date from Local to UTC
+
+    let client = reqwest::Client::new();
+    let ge_rs_response: std::Vec<RSCurrencyRateResponse> = client.get(URL)
+            .query(&[("currencies", "USD"), ("date", date_str)])
+            .send()?
+            .json()?;
+    
+    
+    return validate(ge_rs_response.first(), date)
+
+
+
+fn validate(rs_currency_rate_response: RSCurrencyRateResponse, date: str) -> f64:
+    // validate response so we don't use incorrect date for conversion
+    currency = rs_currency_rate_response.curencies.first();
+
+    assert_eq(currency.validFromDate, date)
+    assert_eq(currency.code, "USD")
+ 
+    return currency
+
+
 
 #[rocket::post("/api/monthly_tax", format = "json",  data="<income>")]
 fn monthly_tax(income: Json<IncomeIn>) -> Json<IncomeOut>{
